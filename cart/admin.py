fd7fa1bffbd5687a01c2fce51cafe0e5ff87daf7
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-07-10 15:42:34 -0300

    I created an Order() and OrderItems() models classes, and OrderAdmin(), and OrderItemsAdmin() and OrderItemsInline() classes

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from cart.models import Order, OrderItems
 
-# Register your models here.
+@admin.register(OrderItems)
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = "order", "product", "variation", "price", "quantity"
+    list_display_links = "order",
+    search_fields = "order", "product",
+    list_per_page = 10
+
+
+class OrderItemsInline(admin.TabularInline):
+    model = OrderItems
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = "pk", "total", "status"
+    list_display_links = "pk",
+    search_fields = "pk", "total", "status",
+    list_per_page = 10
+    ordering = "-pk",
+    inlines = OrderItemsInline,
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Order(models.Model):
+    class Meta:
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
+
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        verbose_name="Usuário"
+    )
+    total = models.FloatField()
+    status = models.CharField(
+        max_length=1,
+        default="C",
+        choices=(
+            ("A", "Aprovado"),
+            ("C", "Criado"),
+            ("R", "Reprovado"),
+            ("P", "Pendente"),
+            ("E", "Enviado"),
+            ("F", "Finalizado"),
+        )
+    )
+
+    def __str__(self) -> str:
+        return f"Pedido N. {self.pk}"
+    
+
+class OrderItems(models.Model):
+    class Meta:
+        verbose_name = "Item do Pedido"
+        verbose_name_plural = "Itens do Pedido"
+
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        verbose_name="Pedido"
+    )
+    product = models.CharField(max_length=120, verbose_name="Produto")
+    product_id = models.PositiveIntegerField(verbose_name="Produto ID")
+    variation = models.CharField(max_length=50, verbose_name="Variação")
+    variation_id = models.PositiveIntegerField(verbose_name="Variacão ID")
+    price = models.FloatField(verbose_name="Preço")
+    promotional_price = models.FloatField(verbose_name="Preço Promocional")
+    quantity = models.PositiveIntegerField(verbose_name="Quantidade")
+    image = models.CharField(max_length=2000)
+
+    def __str__(self) -> str:
+        return f"Item do {self.order}"
